33f4d7aad69e5a8693090d6a8de0b30d088014b6
alpi2111 <carlos.alpi21@gmail.com>
2019-03-18 20:57:44 -0600

    Se agrego la clase de procesos que almacena la ruta y si esta editado el archivo, ademas de validar la salidas con menus

--- EpixCompilerIDE/frmEpixIDE.cs
+++ EpixCompilerIDE/frmEpixIDE.cs
@@ -8,47 +8,61 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace EpixCompilerIDE
-{
-    public partial class frmMain : Form
-    {
-        public frmMain()
-        {
+namespace EpixCompilerIDE {
+    public partial class frmMain : Form {
+        public frmMain() {
             InitializeComponent();
             archGuardar.Enabled = false;
             archCerrar.Enabled = false;
         }
 
-        private void archNuevo_Click(object sender, EventArgs e)
-        {
+        Procesos archivo = new Procesos();
+
+        private void archNuevo_Click(object sender, EventArgs e) {
             //TODO: generar el archivo nuevo sin guardar
         }
 
-        private void archAbrir_Click(object sender, EventArgs e)
-        {
+        private void archAbrir_Click(object sender, EventArgs e) {
             OpenFileDialog openFile = new OpenFileDialog();
-            openFile.Filter = "Epix source file|*.epix";
+            openFile.Filter = "Epix source file|*.txt";
             openFile.ShowDialog();
-            string ruta = openFile.FileName;
-            Console.WriteLine(ruta);
+            //string ruta = openFile.FileName;
+            archivo.Ruta = openFile.FileName;
+            Console.WriteLine(archivo.Ruta);
         }
 
-        private void archGuardar_Click(object sender, EventArgs e)
-        {
+        private void archGuardar_Click(object sender, EventArgs e) {
             //TODO: generar el codigo para guardar un archivo, debe llevar las validaciones
             //para cuando es la primera vez o cuando se actualiza
+            if (archivo.IsSave == false) {
+                //debe guardar el archivo
+            } else {
+                //no debe hacer nada
+            }
         }
 
-        private void archCerrar_Click(object sender, EventArgs e)
-        {
+        private void archCerrar_Click(object sender, EventArgs e) {
             //TODO: generar el cerrado del archivo actual, si no hay no debe estar activo
+            if (archivo.Ruta.Length > 0 && archivo.IsSave == false) { //verifica si existe un archivo y no esta guardado
+                //TODO: decir si guardar el archivo o no guardar el archivo y dependiendo de, cerrarlo
+            } else {
+                //TODO: simplemente cerrar el archivo
+            }
         }
 
-        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //TODO: falta generar el modo de cerrar si hay un archivo sin guardar y
-            //permitir guardar o no.
-            Application.Exit();
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (archivo.IsSave == true || archivo.Ruta.Equals("")) {
+                Application.Exit();
+            } else if (archivo.IsSave == false) {
+                DialogResult salir = MessageBox.Show("Existe un documento sin guardar, ¿Desea guardar los cambios y salir?", "¿Salir?", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (salir == DialogResult.Yes) {
+                    //TODO: guardar cambios y luego cerrar
+                } else if (salir == DialogResult.Cancel) {
+
+                } else {
+                    Application.Exit();
+                }
+            }
         }
     }
 }
---
+++ EpixCompilerIDE/procesos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpixCompilerIDE
+{
+    class Procesos
+    {
+
+        private bool isSave;
+        private String ruta;
+
+        public bool IsSave { get => isSave; set => isSave = value; }
+        public string Ruta { get => ruta; set => ruta = value; }
+
+        public Procesos()
+        {
+            IsSave = false;
+            Ruta = "";
+        }
+    }
+}
