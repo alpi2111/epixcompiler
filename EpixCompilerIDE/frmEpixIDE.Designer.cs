26ad951c7dc23c448c0472c5891e0817970b4e5a
alpi2111 <carlos.alpi21@gmail.com>
2019-03-18 19:37:59 -0600

    Se agrego el menu solo con algunas funciones en archivo

---
+++ EpixCompilerIDE/frmEpixIDE.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace EpixCompilerIDE
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.menuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.archNuevo = new System.Windows.Forms.ToolStripMenuItem();
+            this.archAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.archGuardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.archCerrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.proyectoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajustesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuArchivo,
+            this.editarToolStripMenuItem,
+            this.proyectoToolStripMenuItem,
+            this.ajustesToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(984, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // menuArchivo
+            // 
+            this.menuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archNuevo,
+            this.archAbrir,
+            this.toolStripSeparator1,
+            this.archGuardar,
+            this.toolStripSeparator2,
+            this.archCerrar,
+            this.toolStripSeparator3,
+            this.salirToolStripMenuItem});
+            this.menuArchivo.Name = "menuArchivo";
+            this.menuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.menuArchivo.Text = "Archivo";
+            // 
+            // archNuevo
+            // 
+            this.archNuevo.Name = "archNuevo";
+            this.archNuevo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.archNuevo.Size = new System.Drawing.Size(180, 22);
+            this.archNuevo.Text = "Nuevo";
+            this.archNuevo.Click += new System.EventHandler(this.archNuevo_Click);
+            // 
+            // archAbrir
+            // 
+            this.archAbrir.Name = "archAbrir";
+            this.archAbrir.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.archAbrir.Size = new System.Drawing.Size(180, 22);
+            this.archAbrir.Text = "Abrir";
+            this.archAbrir.Click += new System.EventHandler(this.archAbrir_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // archGuardar
+            // 
+            this.archGuardar.Name = "archGuardar";
+            this.archGuardar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.archGuardar.Size = new System.Drawing.Size(180, 22);
+            this.archGuardar.Text = "Guardar";
+            this.archGuardar.Click += new System.EventHandler(this.archGuardar_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // archCerrar
+            // 
+            this.archCerrar.Name = "archCerrar";
+            this.archCerrar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.archCerrar.Size = new System.Drawing.Size(180, 22);
+            this.archCerrar.Text = "Cerrar";
+            this.archCerrar.Click += new System.EventHandler(this.archCerrar_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(177, 6);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // proyectoToolStripMenuItem
+            // 
+            this.proyectoToolStripMenuItem.Name = "proyectoToolStripMenuItem";
+            this.proyectoToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.proyectoToolStripMenuItem.Text = "Proyecto";
+            // 
+            // ajustesToolStripMenuItem
+            // 
+            this.ajustesToolStripMenuItem.Name = "ajustesToolStripMenuItem";
+            this.ajustesToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.ajustesToolStripMenuItem.Text = "Ajustes";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "frmMain";
+            this.Text = "Epix IDE";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem archNuevo;
+        private System.Windows.Forms.ToolStripMenuItem archAbrir;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem archGuardar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem archCerrar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem proyectoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajustesToolStripMenuItem;
+    }
+}
+
---
+++ EpixCompilerIDE/frmEpixIDE.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EpixCompilerIDE
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            archGuardar.Enabled = false;
+            archCerrar.Enabled = false;
+        }
+
+        private void archNuevo_Click(object sender, EventArgs e)
+        {
+            //TODO: generar el archivo nuevo sin guardar
+        }
+
+        private void archAbrir_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Epix source file|*.epix";
+            openFile.ShowDialog();
+            string ruta = openFile.FileName;
+            Console.WriteLine(ruta);
+        }
+
+        private void archGuardar_Click(object sender, EventArgs e)
+        {
+            //TODO: generar el codigo para guardar un archivo, debe llevar las validaciones
+            //para cuando es la primera vez o cuando se actualiza
+        }
+
+        private void archCerrar_Click(object sender, EventArgs e)
+        {
+            //TODO: generar el cerrado del archivo actual, si no hay no debe estar activo
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO: falta generar el modo de cerrar si hay un archivo sin guardar y
+            //permitir guardar o no.
+            Application.Exit();
+        }
+    }
+}
